f976663ae267da95aa36c5db26dfa1ccd277ccfe
Christian Briscoe <cbriscoe1995@gmail.com>
2017-02-07 15:31:56 -0500

    Added sound play on mouseenter and click

--- script.js
+++ script.js
@@ -1,27 +1,39 @@
+// HTML buttons
 var boomButton = document.getElementById('boom-button')
+var clapButton = document.getElementById('clap-button')
+var hiHatButton = document.getElementById('hi-hat-button')
 
-boomButton.addEventListener('click', function () {
+// Sound files
+var boomSound = document.getElementById('boom-sound')
+var clapSound = document.getElementById('clap-sound')
+var hiHatSound = document.getElementById('hi-hat-sound')
+
+boomButton.addEventListener('click', function(){
+  boomSound.play()
   console.log('Boom click!')
 })
 
 boomButton.addEventListener('mouseenter', function(){
+  boomSound.play()
   console.log('Boom Mouseover')
 })
 
-var clapButton = document.getElementById('clap-button')
 clapButton.addEventListener('click', function () {
+  clapSound.play()
   console.log('Clap!')
 })
 
 clapButton.addEventListener('mouseenter', function(){
+  clapSound.play()
   console.log('Clap Mouseover')
 })
 
-var hiHatButton = document.getElementById('hi-hat-button')
 hiHatButton.addEventListener('click', function () {
+  hiHatSound.play()
   console.log('Whatever sound a hi hat makes!')
 })
 
 hiHatButton.addEventListener('mouseenter', function(){
+  hiHatSound.play()
   console.log('Hi Hat Mouseover')
 })
